ffcd3112b1f9ff9612d247d5b0df631b781aa7a4
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-29 18:31:04 +0000

    add functionality to change the user name

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,43 @@ import MessageList from './MessageList.jsx';
 
 class App extends Component {
 
-  // let users =
-
   constructor(props) {
     super(props);
     this.state = {
-  currentUser: {name: "Reginald"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  currentUser: {name: 'Anonymous'}, // optional. if currentUser is not defined, it means the user is Anonymous
   messages: [
     {
       id: 1,
-      username: "Reginald",
-      content: "Has anyone seen my marbles?",
+      username: 'Reginald',
+      content: 'Has anyone seen my marbles?',
     },
     {
       id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+      username: 'Anonymous',
+      content: 'No, I think you lost them. You lost your marbles Reginald. You lost them for good.'
     }
-  ]
-}}
+    ]
+  },
+  this.onNewPost = this.onNewPost.bind(this);
+  }
 
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({loading: true})  // change the state. this calls render() and the component updates.
-  //   }, 500)
-  // }
+onNewPost(content, username) {
+  const messagesLength = this.state.messages.length + 1;
+
+  setTimeout(() => {
+
+  const newMessage = {id: messagesLength, username: username, content: content};
+  const messages = this.state.messages.concat(newMessage);
+
+  this.setState({messages: messages})
+  }, 500);
+}
 
   render() {
     return (
       <div>
-      <ChatBar user={ this.state.currentUser.name } />
-      <MessageList messages={ this.state.messages }/>
+        <ChatBar onNewPost={ this.onNewPost } />
+        <MessageList messages={ this.state.messages }/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
 
-
-
 class ChatBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.onMessageInput = this.onMessageInput.bind(this);
+    this.onUsernameInput = this.onUsernameInput.bind(this);
+  }
+
+  onMessageInput(event) {
+    this.setState({
+    content: event.target.value
+    });
+  }
+
+  onUsernameInput(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handleKeyPress(event) {
+    // If enter is pressed.
+    if(event.charCode === 13){
+      const currentUser = this.state.username ? this.state.username : 'Anonymous';
+
+      this.props.onNewPost(this.state.content, currentUser);
+      this.setState({ content: '' });
+    }
+  }
+
   render() {
+
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.user} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" onChange={ this.onUsernameInput } />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onChange={ this.onMessageInput } onKeyPress={this.handleKeyPress} />
       </footer>
     );
   }
